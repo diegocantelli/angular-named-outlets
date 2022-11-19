a3d1d5f5f7114e6f7e8b298e5681c08724826877
diegocantelli <diegocantelli19@gmail.com>
2022-11-19 16:47:32 -0300

    Adicionando rota inicial com outlet nomeado padrao

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -3,6 +3,11 @@ import { Routes, RouterModule } from "@angular/router";
 
 const routes: Routes = [
   {
+    path: '',
+    pathMatch: 'full', // verifica a url inteira, ignorando as rotas 'root' de componentes filhos
+    redirectTo: '/(bulb:off)'
+  },
+  {
     path: ':state',
     loadChildren: () => import('./light-bulb/light-bulb.module').then(m => m.LightBulbModule),
     outlet: 'bulb'
